e4705d2d6104e32ebcd612f4d1b2c8bad9965f08
Qiu-Quanzhi <i@qqzhi.cc>
2026-06-25 10:25:53 +0800

    配置文件标准化 & 部分页面重置

--- scripts/filter/checkThemeConfig.js
+++ scripts/filter/checkThemeConfig.js
@@ -1,18 +1,18 @@
 'use strict';
 
 hexo.extend.filter.register('before_post_render', () => {
-        const data = hexo.locals.get('data');
-        const logger = hexo.log;
-        const theme = hexo.theme.config;
-        if (theme.brevity.enable && !data.brevity) {
-            logger.error('\n 启用即刻短文的情况下，请新建 brevity.yaml。');
-            logger.error('\n If brevity is enabled, brevity data must be supplied! \n Please create brevity.yaml.');
-            process.exit(-1);
-        }
-        if (theme.footer.randomlink && !data.links){
-            logger.error('\n 启用随机链接的情况下，请新建 links.yaml。');
-            logger.error('\n If randomlink is enabled, links data must be supplied! \n Please create links.yaml.');
-            process.exit(-1);
-        }
+    const data = hexo.locals.get('data');
+    const logger = hexo.log;
+    const theme = hexo.theme.config;
+    if (theme.logs.enable && !data.logs) {
+        logger.error('\n 启用即刻短文的情况下，请新建 logs.yaml。');
+        logger.error('\n If logs is enabled, logs data must be supplied! \n Please create logs.yaml.');
+        process.exit(-1);
     }
+    if (theme.footer.randomlink && !data.links) {
+        logger.error('\n 启用随机链接的情况下，请新建 links.yaml。');
+        logger.error('\n If randomlink is enabled, links data must be supplied! \n Please create links.yaml.');
+        process.exit(-1);
+    }
+}
 );
